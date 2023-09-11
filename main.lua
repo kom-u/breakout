7da57649a5b05f19d28751b80f6b59a8910a6b01
Atqa Munzir <atqamz@gmail.com>
2023-09-11 10:33:53 +0700

    initalize project

---
+++ main.lua
@@ -0,0 +1,69 @@
+require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+
+    math.randomseed(os.time())
+
+    love.window.setTitle("Breakout")
+
+    _G.gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+    }
+    love.graphics.setFont(_G.gFonts['small'])
+
+    _G.gTextures = {
+        ['background'] = love.graphics.newImage('graphics/background.png'),
+        ['main'] = love.graphics.newImage('graphics/breakout.png'),
+        ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+        ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+        ['particle'] = love.graphics.newImage('graphics/particle.png')
+    }
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    _G.gSounds = {
+        ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+        ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+        ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+        ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+        ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+        ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+        ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+        ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+
+        ['music'] = love.audio.newSource('sounds/music.wav', 'static')
+    }
+
+    -- the state machine we'll be using to transition between various states
+    -- in our game instead of clumping them together in our update and draw
+    -- methods
+    --
+    -- our current game state can be any of the following:
+    -- 1. 'start' (the beginning of the game, where we're told to press Enter)
+    -- 2. 'paddle-select' (where we get to choose the color of our paddle)
+    -- 3. 'serve' (waiting on a key press to serve the ball)
+    -- 4. 'play' (the ball is in play, bouncing between paddles)
+    -- 5. 'victory' (the current level is over, with a victory jingle)
+    -- 6. 'game-over' (the player has lost; display score and allow restart)
+    _G.gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end
+    }
+    _G.gStateMachine:change('start')
+
+    -- a table we'll use to keep track of which keys have been pressed this
+    -- frame, to get around the fact that LÖVE's default callback won't let us
+    -- test for input from within other functions
+    love.keyboard.keysPressed = {}
+end
